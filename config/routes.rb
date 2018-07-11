ac54f0717d50f34bba25e45715d3053ad2447776
Trịnh Hữu Vũ <vutrinhhuu@gmail.com>
2018-07-11 11:13:21 +0700

    Chapter 7: Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
-  get "/signup", to: "user#new"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
   end
 end
